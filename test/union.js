14386c4e384771f89fdc892dc99e1b552c60a4a8
Bryce Baril <bryce@ravenwall.com>
2013-09-21 21:38:25 -0700

    Adding more tests

--- test/union.js
+++ test/union.js
@@ -25,17 +25,81 @@ test("union", function (t) {
       {"R":4,"_t":3500},
       {"L":4,"_t":4000},
       {"R":5,"_t":4250},
-      {"L":5,"R":6,"_t":5000}
+      {"L":5,"R":6,"_t":5000},
+      {"R":7,"_t":5750}
     ]
     t.deepEquals(records, expected, "Got expected records")
   }
 
   // L:(0,0)       (1,1000)         (2,2000)        (3,3000)        (4,4000)        (5,5000)
-  // R:     (0,500)         (1,1250)(2,2000)(3,2750)        (4,3500)        (5,4250)(6,5000)
+  // R:     (0,500)         (1,1250)(2,2000)(3,2750)        (4,3500)        (5,4250)(6,5000)(7,5750)
   var left = gen({start: 0, until: 5000, interval: 1000, key: "L"})
-  var right = gen({start: 500, until: 5500, interval: 750, key: "R"})
+  var right = gen({start: 500, until: 5800, interval: 750, key: "R"})
 
   union("_t", left, right)
+    .pipe(concat(collect))
+})
+
+test("union different lengths", function (t) {
+  t.plan(1)
+
+  function collect(records) {
+    var expected = [
+      {"L":0,"_t":0},
+      {"R":0,"_t":500},
+      {"L":1,"_t":1000},
+      {"R":1,"_t":1250},
+      {"L":2,"R":2,"_t":2000},
+      {"R":3,"_t":2750},
+      {"L":3,"_t":3000},
+      {"R":4,"_t":3500},
+      {"L":4,"_t":4000},
+      {"R":5,"_t":4250},
+      {"L":5,"_t":5000}
+    ]
+    t.deepEquals(records, expected, "Got expected records")
+  }
+
+  // L:(0,0)       (1,1000)         (2,2000)        (3,3000)        (4,4000)        (5,5000)
+  // R:     (0,500)         (1,1250)(2,2000)(3,2750)        (4,3500)        (5,4250)
+  var left = gen({start: 0, until: 5000, interval: 1000, key: "L"})
+  var right = gen({start: 500, until: 4500, interval: 750, key: "R"})
+
+  union("_t", left, right)
+    .pipe(concat(collect))
+})
+
+test("union three streams", function (t) {
+  t.plan(1)
+
+  function collect(records) {
+    var expected = [
+      {"L":0,"_t":0,"N":0},
+      {"R":0,"_t":500},
+      {"L":1,"_t":1000},
+      {"R":1,"_t":1250},
+      {"N":1,"_t":1500},
+      {"L":2,"R":2,"_t":2000},
+      {"R":3,"_t":2750},
+      {"L":3,"_t":3000,"N":2},
+      {"R":4,"_t":3500},
+      {"L":4,"_t":4000},
+      {"R":5,"_t":4250},
+      {"N":3,"_t":4500},
+      {"L":5,"_t":5000}
+    ]
+    t.deepEquals(records, expected, "Got expected records")
+  }
+
+  // L:(0,0)       (1,1000)                 (2,2000)        (3,3000)        (4,4000)                (5,5000)
+  // R:     (0,500)         (1,1250)        (2,2000)(3,2750)        (4,3500)        (5,4250)
+  // N:(0,0)                        (1,1500)                (2,3000)                        (4,4500)
+  var left = gen({start: 0, until: 5000, interval: 1000, key: "L"})
+  var right = gen({start: 500, until: 4500, interval: 750, key: "R"})
+  var next = gen({start: 0, until: 4500, interval: 1500, key: "N"})
+
+  var first = union("_t", left, right)
+  union("_t", first, next)
     .pipe(concat(collect))
 })
 
